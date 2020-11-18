14bd894147747b828c53ea6c699eafb0e8cdd1f2
giannispan <giannis.panagiotopoulos@gmail.com>
2020-11-18 15:24:55 +0200

    prod deploy prepare

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
 
 // Connect Database
@@ -8,14 +10,19 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-	res.json({msg: 'Welcome to contact keeper API'}));
-
-
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req,res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
